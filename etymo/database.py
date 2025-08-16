a6ad87d258b04dac5fd9facfed6d101e95f46a99
sawantsanket855 <sawantsanket855@gmail.com>
2025-08-17 01:48:40 +0530

    commit for developement 2

---
+++ etymo/database.py
@@ -0,0 +1,24 @@
+from django.db import connection
+from django.http import HttpResponse,JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+
+def check_connection(request):
+    with connection.cursor() as cursor:
+        cursor.execute("select * from student")
+        rows=cursor.fetchall()
+        print(rows)
+        return HttpResponse(rows[0][1])
+
+@csrf_exempt
+def get_word_data(request):
+    words_list = request.GET.getlist('highlightedWords')
+    print(words_list)
+    words=tuple(words_list)
+    # words=('aspiration','asthma','bradycardia')
+    with connection.cursor() as cursor:
+        cursor.execute(f"select * from tbl_medical_terms where col_medical_term in {words}")
+        rows=cursor.fetchall()
+        return JsonResponse({'results':rows})
+
+
+
