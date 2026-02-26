6d5be8e268d58e6e0d00ef318eb1a682a74c0874
sawantsanket855 <sawantsanket855@gmail.com>
2026-02-26 10:31:57 +0530

    ready for demo test 4

--- etymo/demo.py
+++ etymo/demo.py
@@ -2,4 +2,4 @@ import requests
 url = "https://5174b1de1fac.ngrok-free.app/api/"
 headers = {"ngrok-skip-browser-warning": "true"}
 r = requests.get(url, headers=headers)
-print(r.text)
+print(r.text) 
